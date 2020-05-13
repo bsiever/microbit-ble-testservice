f33217c5cb6e4d3e3729beb5d509a857faadd92a
Bill Siever <bsiever@gmail.com>
2020-05-12 19:20:49 -0500

    Updates / works with older nordic library.

--- src/main.cpp
+++ src/main.cpp
@@ -8,29 +8,31 @@ MicroBit uBit;
 
 void onConnected(MicroBitEvent) {
     uBit.display.scroll("O");
+    uBit.serial.printf("Connected\n");
 }
 
 void onDisconnected(MicroBitEvent) {
     uBit.display.scroll("X");
+    uBit.serial.printf("Disconnected\n");
 }
 
 int main() {
     uBit.init();
+
+    uBit.display.scroll("Y");
+    uBit.serial.printf("Start......\n");
+
     uBit.messageBus.listen(MICROBIT_ID_BLE, MICROBIT_BLE_EVT_CONNECTED, onConnected);
     uBit.messageBus.listen(MICROBIT_ID_BLE, MICROBIT_BLE_EVT_DISCONNECTED, onDisconnected);
-
     bleTest = new BLETestService(*uBit.ble);
     uBit.bleManager.advertise();
-    uBit.display.scroll("X");
-    // uBit.sleep(2000);
-    // uBit.serial.printf("Hello7\n");
-    // uBit.display.printChar('H');
-    // uBit.bleManager.advertise();
-    //  for(int i=0;i<100;i++)
-    //    uBit.serial.send("OK!!!!\n");
 
-// TODOs:  Figure out task problem;  Figure out crash / halt issue; 
-
+    int i=0;
+    while(true) {
+        uBit.sleep(1000);
+        uBit.display.scroll(i);
+        uBit.serial.printf("Count 3: %d\n",i++);
+    }
 
     // If main exits, there may still be other fibers running or registered event handlers etc.
     // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
